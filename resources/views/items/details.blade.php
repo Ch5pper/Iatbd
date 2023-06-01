a02295f609db732d91e5866cc92406c78ff0d27c
Perry J.R. Rijaard <93517178+Ch5pper@users.noreply.github.com>
2023-06-01 15:33:35 +0200

    Style to the details pages

--- resources/views/items/details.blade.php
+++ resources/views/items/details.blade.php
@@ -5,19 +5,54 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Product Details</title>
-</head>
-<style>
-/* Your CSS styles here */
-</style>
-<body>
-<div class="navbar">
-    <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-        <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-            {{ __('Ga terug') }}
-        </x-nav-link>
-    </div>
-</div>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
 
+        .navbar {
+            background-color: #f1f1f1;
+            padding: 10px;
+        }
+
+        .navbar a {
+            color: #000;
+            text-decoration: none;
+            margin-right: 8px;
+        }
+
+        h1 {
+            font-size: 24px;
+            margin-top: 20px;
+            margin-bottom: 10px;
+        }
+
+        .product-details {
+            background-color: #f9f9f9;
+            padding: 20px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+        }
+
+        .product-details h2 {
+            font-size: 18px;
+            margin-bottom: 10px;
+        }
+
+        .product-details p {
+            font-size: 14px;
+            color: #555;
+        }
+    </style>
+</head>
+<body>
+    <div class="navbar">
+        <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+            <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                {{ __('Ga terug') }}
+            </x-nav-link>
+        </div>
+    </div>
 
     <h1>Product Details</h1>
 
